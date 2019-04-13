3767dbc676a1e118940e5fa7a558d4f015fb33f4
Chris Paika <paika.christopher@gmail.com>
2019-04-13 09:08:02 -0400

    Returning basic hello

--- src/main/java/com/chrispaika/tagwatchersvc/v1/MainController.java
+++ src/main/java/com/chrispaika/tagwatchersvc/v1/MainController.java
@@ -3,14 +3,19 @@ package com.chrispaika.tagwatchersvc.v1;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
 
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
     public ResponseEntity<String> sayHello() {
-        ResponseEntity<String> responseString = new ResponseEntity<String>("Hello", HttpStatus.OK);
+        ResponseEntity<String> responseString = new ResponseEntity<String>("Hello!", HttpStatus.OK);
         return responseString;
     }
 
