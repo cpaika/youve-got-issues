0f307a06faa506102825fd8bc1676d89a80f4ece
Chris Paika <paika.christopher@gmail.com>
2019-04-12 20:24:19 -0400

    Adding service

---
+++ src/main/java/com/chrispaika/tagwatchersvc/TagWatcherSvcApplication.java
@@ -0,0 +1,13 @@
+package com.chrispaika.tagwatchersvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagWatcherSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagWatcherSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrispaika/tagwatchersvc/TagWatcherSvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chrispaika.tagwatchersvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TagWatcherSvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
